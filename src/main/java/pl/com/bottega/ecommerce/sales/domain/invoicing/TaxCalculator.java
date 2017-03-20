8672c23cf9cbf59f66ac1c27e7b1fc13ad3fab4d
Khandree <a.lis.marciniak@gmail.com>
2017-03-20 21:36:34 +0100

    Added a new class TaxCreator

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TaxCalculator.java
@@ -0,0 +1,25 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class TaxCalculator {
+    public Tax calculateTax (ProductType type, Money price){
+        TaxCreator creator;
+        switch (type) {
+            case DRUG:
+                creator = new DrugTaxCreator();
+                break;
+            case FOOD:
+                creator = new FoodTaxCreator();
+                break;
+            case STANDARD:
+                creator = new StandardTaxCreator();
+                break;
+
+            default:
+                throw new IllegalArgumentException(type + " not handled");
+        }
+        return creator.createTax(price);
+    }
+}
